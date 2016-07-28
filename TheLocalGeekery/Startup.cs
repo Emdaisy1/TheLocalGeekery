5077f88c63d7d6812c84720bfb3d650408a9114d
Emdaisy1 <hilborn_emmav@live.ca>
2016-07-27 21:22:21 -0400

    Add project files.

---
+++ TheLocalGeekery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheLocalGeekery.Startup))]
+namespace TheLocalGeekery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
